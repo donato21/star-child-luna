b59bc71dbcc53f69351c6124521465e494b5e77b
donato21 <donatoisai16@gmail.com>
2022-08-14 09:12:55 -0500

    Working temp Lvl1 and Player mvmt

---
+++ entities/player/Player.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+signal player_dead
+signal area_entered
+signal item_equiped
+
+export var speed : int = 50
+var dir : Vector2 = Vector2.ZERO
+var vel : Vector2 = Vector2.ZERO
+var facing : Vector2 = Vector2.ZERO
+
+func _input(_event):
+	dir = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		dir.x -= 1
+		facing = Vector2.LEFT
+	if Input.is_action_pressed("right"):
+		dir.x += 1
+		facing = Vector2.RIGHT
+	if Input.is_action_pressed("forward"):
+		dir.y -= 1
+		facing = Vector2.UP
+	if Input.is_action_pressed("back"):
+		dir.y += 1
+		facing = Vector2.DOWN
+
+func _process(_delta):
+	if facing == Vector2.UP:
+		if vel.y < 0:
+			$AnimatedSprite.play("walkF")
+		if vel.y == 0:
+			$AnimatedSprite.play("idleF")
+	if facing == Vector2.DOWN:
+		if vel.y > 0:
+			$AnimatedSprite.play("walkB")
+		if vel.y == 0:
+			$AnimatedSprite.play("idleB")
+	if facing == Vector2.LEFT:
+		if vel.x < 0:
+			$AnimatedSprite.play("walkL")
+		if vel.x == 0:
+			$AnimatedSprite.play("idleL")
+	if facing == Vector2.RIGHT:
+		if vel.x > 0:
+			$AnimatedSprite.play("walkR")
+		if vel.x == 0:
+			$AnimatedSprite.play("idleR")
+	
+	vel = move_and_slide(speed * dir)
--- menus/MainMenu.gd
+++ menus/MainMenu.gd
@@ -5,16 +5,12 @@ signal load_scene
 func _on_Play_pressed():
 	emit_signal("load_scene", "res://levels/Level1.tscn")
 
-
 func _on_Quit_pressed():
 	get_parent().get_tree().quit()
-
 
 func _on_Options_pressed():
 	$CanvasLayer/OptionsPopUp.play("show")
 	$CanvasLayer/PopUpTimer.start()
-
-
 
 func _on_PopUpTimer_timeout():
 	$CanvasLayer/OptionsPopUp.play("hide")
