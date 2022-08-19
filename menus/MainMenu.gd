a9f8d0654bb8e225522602dc5351a837f2f2c453
donato21 <donatoisai16@gmail.com>
2022-08-19 16:23:03 -0500

    changed .gitignore and mainmenu

--- menus/MainMenu.gd
+++ menus/MainMenu.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
 signal load_scene
+signal play_audio
+
+export var menu_theme_path: String
+export var on_play_scene_path: String = "res://levels/Level1.tscn"
+
+func _ready():
+	emit_signal("play_audio", {"path": menu_theme_path, "type":"loop"})
 
 func _on_Play_pressed():
-	emit_signal("load_scene", "res://levels/Level1.tscn")
+	emit_signal("load_scene", on_play_scene_path)
 
 func _on_Quit_pressed():
 	get_parent().get_tree().quit()
