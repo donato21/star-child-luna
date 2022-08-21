bc0c5ed287e6b42da7c793429397d952e759b607
donato21 <donatoisai16@gmail.com>
2022-08-20 23:59:02 -0500

    added animation controller for GUI

--- Snake.gd
+++ Snake.gd
@@ -1,19 +1,22 @@
 extends Node2D
 
-export var min_sleep_time: float = 3
-export var max_sleep_time: float = 6
-export var min_wake_time: float = 2
-export var max_wake_time: float = 4
-export var deay: float = 0
+var min_sleep_time: float = 3
+var max_sleep_time: float = 6
+var min_wake_time: float = 2
+var max_wake_time: float = 4
+export var delay: float = 0
 export var awake_sound_path: String = ""
 export var sleep_sound_path: String = ""
 
 var sleeping = true
 
+signal play_audio
+signal game_over
+
 func _on_SleepTimer_timeout():
 	if sleeping:
 		$Timer.set_wait_time(rand_range(min_wake_time, max_wake_time))
-		$DeathCircle.visible = true
+		$Area2D/DeathCircle.visible = true
 		$Timer.start()
 		$AnimatedSprite.play("awake")
 		for body in $Area2D.get_overlapping_bodies():
@@ -22,7 +25,7 @@ func _on_SleepTimer_timeout():
 		sleeping = false
 	else:
 		$Timer.set_wait_time(rand_range(min_sleep_time, max_sleep_time))
-		$DeathCircle.visible = false
+		$Area2D/DeathCircle.visible = false
 		$Timer.start()
 		$AnimatedSprite.play("asleep")
 		sleeping = true
@@ -33,3 +36,4 @@ func _on_Area2D_body_entered(body):
 
 func kill_player():
 	print("player should be dead...")
+	emit_signal("game_over","snake")
